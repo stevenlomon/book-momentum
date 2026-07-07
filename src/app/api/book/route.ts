f8e9edb06184bf5aec3949ed231e916ff09362c8
StevenLomon <steven.cesario@proton.me>
2026-07-07 09:22:51 +0200

    improvement to Book POST Route Handler: now checks if the user is logged + we use upsert instead of insert

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,16 +1,30 @@
 // Our own internal book API that will be used to create "local copies" of Open Libary books whenever a user adds them to their Bookshelf
 import { NextResponse } from 'next/server';
 import { pool } from '@/lib/db';
+import { getCurrentUser } from '@/lib/auth';
 
 export async function POST(req: Request) {
   try {
+    // Security upgrade: Check if user is logged in before letting them write to the DB
+    const user = await getCurrentUser();
+    if (!user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
     const body = await req.json();
 
     // The server provides all data except id that Postgres can automatically generate. It's here that I realize that I can't possibly
     // include goodreads_url since the URL contains an id that I can't possibly know haha. The row is now deleted from the table
     const query = {
-      name: 'insert-open-library-book',
-      text: 'INSERT INTO "Book"(title, author, external_provider, external_id, page_count, cover_image_url) VALUES($1, $2, $3, $4, $5, $6) RETURNING *',
+      name: 'insert-open-library-book', // Upgrade: insert to upsert!
+      // We now check if the book has already been added from Open Library with ON CONFLICT. DO UPDATE SET forces Postgres to return the row if it already existed
+      text: `
+        INSERT INTO "Book"(title, author, external_provider, external_id, page_count, cover_image_url) 
+        VALUES($1, $2, $3, $4, $5, $6) 
+        ON CONFLICT (external_id) 
+        DO UPDATE SET external_id = EXCLUDED.external_id
+        RETURNING *
+      `,
       values: [body.title, body.author, body.external_provider, body.external_id, body.page_count, body.cover_image_url]
     }
     const res = await pool.query(query);
